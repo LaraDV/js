275f79369213b4f35a46f2b02d417e70e898e533
LaraDV <d.lara.v@bk.ru>
2020-04-24 22:51:47 +0300

    styles added,destructuring used,commas removed

--- js/main.js
+++ js/main.js
@@ -5,24 +5,25 @@ const products = [
   {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title, price, img="placeimg.com") => {
   return `<div class="product-item">
             <h3>${title}</h3>
-            <p>${price}</p>
+            <img src=https://${img}/220/160/tech>
+            <p class="product-price">${price}</p>
             <button class="by-btn">Добавить в корзину</button>
           </div>`;
 };
-
-const renderProducts = (list) => {
-  // const productList = list.map((good) => {
-  //   return renderProduct(good.title, good.price);
-  // });
-  const productList = [];
-  list.forEach(good => {
-    productList.push(renderProduct(good.title, good.price));
+/*const renderProducts = (list) => {
+  const productList = list.map((good) => {
+    return renderProduct(good.title, good.price);
   });
-  console.log(productList);
   document.querySelector('.products').innerHTML = productList;
-};
+}
+renderProducts(products);*/
 
-renderProducts(products);
+products.forEach(good => {
+  let {title, price} = good;
+  let product = renderProduct(title, price); 
+  document.querySelector('.products').insertAdjacentHTML('beforeend',product);
+});
+
